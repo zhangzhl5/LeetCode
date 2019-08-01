0fa61f85647b3c2577fe1f10a1a04e813927ea8d
zhangzhl5 <zhangzhl5@yonyou.com>
2019-08-01 20:34:09 +0800

    thread

---
+++ src/com/zhangzhl/thread/ITask.java
@@ -0,0 +1,9 @@
+package com.zhangzhl.thread;
+
+public interface ITask {
+	
+	public Boolean getPen();
+    
+	public void openPainting();
+
+}
---
+++ src/com/zhangzhl/thread/Painting.java
@@ -0,0 +1,7 @@
+package com.zhangzhl.thread;
+
+public class Painting {
+	
+	public static boolean pen = true;
+
+}
