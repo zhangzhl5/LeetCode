54572755e05b4cf071e9007d1a26bf529adac669
zhangzhl5 <zhangzhl5@yonyou.com>
2019-08-27 14:51:44 +0800

    双指针法

--- src/com/zhangzhl/invoke/test/TestAspect.java
+++ src/com/zhangzhl/invoke/test/TestAspect.java
@@ -9,9 +9,9 @@ public class TestAspect {
     public static void main(String[] args){
         DynProxyImpl dynProxy = new DynProxyImpl();
         BookDao bookDao = new BookDaoImpl();
-        BookDao bookDao1 = (BookDao)dynProxy.creatProxy(bookDao);
-        bookDao1.addBook("java");
-        bookDao1.sendBook(0);
-        bookDao1.deletBook("java");
+//        BookDao bookDao1 = (BookDao)dynProxy.creatProxy(bookDao);
+//        bookDao1.addBook("java");
+//        bookDao1.sendBook(0);
+//        bookDao1.deletBook("java");
     }
 }
--- src/com/zhangzhl/title/numSubrrayProductLessThank.java
+++ src/com/zhangzhl/title/numSubrrayProductLessThank.java
@@ -20,7 +20,19 @@ package com.zhangzhl.title;
  */
 
 public class numSubrrayProductLessThank {
-
+	
+	
+	public static void main(String[] args) {
+		numSubrrayProductLessThank  s = new numSubrrayProductLessThank();
+		int[] arr = {10,5,2,6};
+		int num = s.numSubarrayProductLessThanK(arr,100);
+		System.out.println(num);
+	}
+	//双指针法，如果一个子串的乘积小于k，那么他的每个子集都小于k，而一个长度为n的数组，
+	//他的所有连续子串数量是1+2+...n，但是会和前面的重复。 比如例子中[10, 5, 2, 6]，
+	//第一个满足条件的子串是[10]，第二个满足的是[10, 5]，但是第二个数组的子集[10]和前面的已经重复了，
+	//因此我们只需要计算包含最右边的数字的子串数量，就不会重复了，也就是在计算[10, 5]这个数组的子串是，
+	//只加入[5]和[10, 5]，而不加入[10]，这部分的子串数量刚好是r - l + 1
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         if ( k <= 1) return 0;
 
@@ -28,12 +40,15 @@ public class numSubrrayProductLessThank {
         long p = 1l;
         int i = 0, total = 0;
         for (int j = 0; j < n; j++){
+        	System.out.println("本数为"+nums[j]);
             p *= nums[j];
+            System.out.println("本乘数为"+p);
             while (p >= k){
                 p /= nums[i];
                 i++;
             }
             total += (j - i + 1);
+            System.out.println("个数为"+total);
         }
 
         return total;
