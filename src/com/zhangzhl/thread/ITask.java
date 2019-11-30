c073509f949f4b38c9842de8cecee200c752133d
zhangzhl5 <zhangzhl5@yonyou.com>
2019-11-30 16:31:39 +0800

    删除旧代码

--- src/com/zhangzhl/thread/ITask.java
+++
@@ -1,9 +0,0 @@
-package com.zhangzhl.thread;
-
-public interface ITask {
-	
-	public Boolean getPen();
-    
-	public void openPainting();
-
-}
--- src/com/zhangzhl/thread/Painting.java
+++
@@ -1,7 +0,0 @@
-package com.zhangzhl.thread;
-
-public class Painting {
-	
-	public static boolean pen = true;
-
-}
