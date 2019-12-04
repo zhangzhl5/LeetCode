ade481c83acb2a08a5e86645acbfea5db5ef4a58
zhangzhl5 <zhangzhl5@yonyou.com>
2019-12-04 20:50:47 +0800

    最长汇文

---
+++ src/com/zhangzhl/title/LongestPalindrome.java
@@ -0,0 +1,14 @@
+package com.zhangzhl.title;
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。 
+ * 输入: "babad" 输出: "bab" 
+ * 注意: "aba" 也是一个有效答案。
+ */
+public class LongestPalindrome {
+	
+	public String longestPalindrome(String s) {
+		return s;
+
+	}
+}
