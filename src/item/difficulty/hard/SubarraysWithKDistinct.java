56734d6f92661a9dc0f725e3fbb4283742808d78
zhangzhl <185378697@qq.com>
2020-01-04 23:27:43 +0800

    K个不同整数的子数组
    Change-Id: I5c1afdf5014de1a8a45ddcdccbab589df65b131c
    Signed-off-by: zhangzhl <185378697@qq.com>

---
+++ src/item/difficulty/hard/SubarraysWithKDistinct.java
@@ -0,0 +1,12 @@
+package item.difficulty.hard;
+
+/**
+ * 给定一个正整数数组A，如果A的某个子数组中不同整数的个数恰好为K，则称A的这个连续、不一定独立的子数组为好子数组。
+ * 
+ * （例如，[1,2,3,1,2] 中有 3 个不同的整数：1，2，以及 3。）返回 A 中好子数组的数目
+ *   @author zhangzhl
+ *
+ */
+public class SubarraysWithKDistinct {
+
+}
