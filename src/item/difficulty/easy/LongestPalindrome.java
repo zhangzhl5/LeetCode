e05f54c6469f26164a1abe530ca4d8b2802e9e4b
zhangzhl5 <zhangzhl5@yonyou.com>
2020-01-07 10:07:53 +0800

    a

--- src/item/difficulty/easy/LongestPalindrome.java
+++ src/item/difficulty/easy/LongestPalindrome.java
@@ -1,10 +1,5 @@
 package item.difficulty.easy;
 
-/**
- * ����һ���ַ��� s���ҵ� s ����Ļ����Ӵ�������Լ��� s ����󳤶�Ϊ 1000�� 
- * ����: "babad" ���: "bab" 
- * ע��: "aba" Ҳ��һ����Ч�𰸡�
- */
 public class LongestPalindrome {
 	
 	public String longestPalindrome(String s) {
