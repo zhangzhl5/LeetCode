240d10e50cf3f3b40a7b93d2da74c0f5b9df5cd4
zhangzhl <185378697@qq.com>
2020-03-02 23:10:21 +0800

    反转链表
    Change-Id: Ie5e4b81a3db9af72ce70ee4e57eae27bbac431a4
    Signed-off-by: zhangzhl <185378697@qq.com>

--- src/item/difficulty/easy/ReverseList.java
+++ src/item/difficulty/easy/ReverseList.java
@@ -9,12 +9,19 @@ package item.difficulty.easy;
 public class ReverseList {
 
 	 public ListNode reverseList(ListNode head) {
-		 
-		 ListNode temp = head;
-		 while(head.next != null) {
-			 
+		 ListNode prev = null; //前指针节点
+	     ListNode curr = head; //当前指针节点
+	   //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
+		 while(curr != null) {
+			 ListNode nextTemp = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
+			 // 将当前节点的next之前前一个节点，反转过程
+			 curr.next = prev;
+			 // 后移一个节点
+			 prev = curr;
+			 curr = nextTemp;
 		 }
-		return head;
+		 // 当前指针节点为空时说明已经到链表尾部的节点的next指针处
+		return prev;
 	        
 	 }
 }
