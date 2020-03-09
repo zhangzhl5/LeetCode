60548430457183372549906bcdeea35e2a6e7137
zhangzhl <185378697@qq.com>
2020-03-09 22:54:20 +0800

    买卖股票
    Change-Id: If3d0e6497b351a5ffa33b3ab62d28d194232b2c1
    Signed-off-by: zhangzhl <185378697@qq.com>

--- src/item/difficulty/easy/MaxProfit.java
+++ src/item/difficulty/easy/MaxProfit.java
@@ -1,5 +1,22 @@
 package item.difficulty.easy;
 
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。 
+ * 注意你不能在买入股票前卖出股票
+ *
+ */
 public class MaxProfit {
-
+	
+	public int maxProfit(int[] prices) {
+		int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
 }
