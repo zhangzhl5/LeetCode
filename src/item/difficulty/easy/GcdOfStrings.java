dfd39118ae0c2158865b7dfca1dbfe38d8bdc94e
zhangzhl5 <zhangzhl5@yonyou.com>
2020-03-12 12:31:07 +0800

    最大公约数

---
+++ src/item/difficulty/easy/GcdOfStrings.java
@@ -0,0 +1,22 @@
+package item.difficulty.easy;
+
+/**
+ * 字符串的最大公因子 对于字符串 S 和 T，只有在 S = T + ... + T（T 与自身连接 1 次或多次）时，我们才认定 “T 能除尽 S”。
+ * 返回最长字符串 X，要求满足 X 能除尽 str1 且 X 能除尽 str2。
+ *
+ */
+public class GcdOfStrings {
+	public String gcdOfStrings(String str1, String str2) {
+
+		// 假设str1是N个x，str2是M个x，那么str1+str2肯定是等于str2+str1的。
+		if (!(str1 + str2).equals(str2 + str1)) {
+			return "";
+		}
+		// 辗转相除法求gcd。
+		return str1.substring(0, gcd(str1.length(), str2.length()));
+	}
+
+	private int gcd(int a, int b) {
+		return b == 0 ? a : gcd(b, a % b);
+	}
+}
