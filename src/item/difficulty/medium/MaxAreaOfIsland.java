b8b7a12f2ca49ba7aceb5fa3860af733bcda8c76
zhangzhl <185378697@qq.com>
2020-03-15 00:38:18 +0800

    岛屿的最大面积
    Change-Id: If40073ec0e3ab6577691520ad4eada1b81817433
    Signed-off-by: zhangzhl <185378697@qq.com>

---
+++ src/item/difficulty/medium/MaxAreaOfIsland.java
@@ -0,0 +1,13 @@
+package item.difficulty.medium;
+
+/**
+ * 给定一个包含了一些 0 和 1的非空二维数组 grid , 一个 岛屿 是由四个方向 (水平或垂直) 的 1 (代表土地)
+ * 构成的组合。你可以假设二维矩阵的四个边缘都被水包围着。 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为0。)
+ */
+public class MaxAreaOfIsland {
+	
+	 public int maxAreaOfIsland(int[][] grid) {
+		return 0;
+
+	 }
+}
