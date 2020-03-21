bd90599d9b5a0804f3c660bd536f72ff895d88f0
zhangzhl <185378697@qq.com>
2020-03-21 22:21:58 +0800

    辗转相除法 Greatest Common Divisor（最大公约数）
    Change-Id: Iaddd5df13924aa1b241800c86d6e011b62871c30
    Signed-off-by: zhangzhl <185378697@qq.com>

--- src/item/difficulty/medium/CanMeasureWater.java
+++ src/item/difficulty/medium/CanMeasureWater.java
@@ -15,14 +15,24 @@ public class CanMeasureWater {
         }
         // 特判：x+y<z时一定是false！！！ 仔细品一品
         if (x + y < z) return false;
-        // 辗转相除法  是不是特别眼熟 3.12的每日一题 -> 1071. 字符串的最大公因子
+        // 辗转相除法求最大公约数
         int tmp = gcd(x, y);
         return z % tmp == 0;
     }
 
+	/**
+	 * 方法描述 ；辗转相除法 Greatest Common Divisor（最大公约数）
+	 * 用较大数除以较小数，再用出现的余数（第一余数）去除除数，再用出现的余数（第二余数）去除第一余数，如此反复，直到最后余数是0为止。
+	 * 如果是求两个数的最大公约数，那么最后的除数就是这两个数的最大公约数。 
+	 * Greatest Common Divisor(
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	private int gcd(int x, int y) {
 		
 		if (y == 0) return x;
+		
 		return gcd(y,x % y);
 	}
 }
