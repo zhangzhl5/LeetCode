75c634286ccab42973c232ffa682baafa0934fea
zhangzhl <185378697@qq.com>
2020-03-21 22:35:10 +0800

    反转链表
    Change-Id: I671181953c0e439a3415d7946ca4d2ba67f8b01d
    Signed-off-by: zhangzhl <185378697@qq.com>

--- src/item/difficulty/easy/ReverseList.java
+++ src/item/difficulty/easy/ReverseList.java
@@ -2,6 +2,7 @@ package item.difficulty.easy;
 
 
 /**
+ * 面试题24：反转链表
  * 反转一个单链表。
  * 输入: 1->2->3->4->5->NULL
  * 输出: 5->4->3->2->1->NULL
@@ -11,9 +12,9 @@ public class ReverseList {
 	 public ListNode reverseList(ListNode head) {
 		 ListNode prev = null; //前指针节点
 	     ListNode curr = head; //当前指针节点
-	   //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
+	     //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
 		 while(curr != null) {
-			 ListNode nextTemp = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
+			 ListNode nextTemp = curr.next;
 			 // 将当前节点的next之前前一个节点，反转过程
 			 curr.next = prev;
 			 // 后移一个节点
