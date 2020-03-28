138108842dae901c4f442e790fe526aecbb51e00
zhangzhl <185378697@qq.com>
2020-03-29 01:09:31 +0800

    多线程
    Change-Id: I60a8392325c3b8610ee89de57a0102a7fdbeadb4
    Signed-off-by: zhangzhl <185378697@qq.com>

---
+++ src/item/difficulty/easy/Foo.java
@@ -0,0 +1,46 @@
+package item.difficulty.easy;
+
+public class Foo {
+
+	private boolean firstFinished;
+	private boolean secondFinished;
+	Object lock = new Object();
+
+	public Foo() {
+
+	}
+
+	public void first(Runnable printFirst) throws InterruptedException {
+
+		// printFirst.run() outputs "first". Do not change or remove this line.
+		synchronized (lock) {
+			printFirst.run();
+			firstFinished = true;
+			lock.notifyAll();
+		}
+	}
+
+	public void second(Runnable printSecond) throws InterruptedException {
+		synchronized (lock) {
+			while (!firstFinished) {
+				lock.wait();
+			}
+			// printSecond.run() outputs "second". Do not change or remove this line.
+			printSecond.run();
+			secondFinished = true;
+			lock.notifyAll();
+
+		}
+	}
+
+	public void third(Runnable printThird) throws InterruptedException {
+		synchronized (lock) {
+			while (!secondFinished) {
+				lock.wait();
+			}
+			// printSecond.run() outputs "second". Do not change or remove this line.
+			printThird.run();
+		}
+	}
+
+}
