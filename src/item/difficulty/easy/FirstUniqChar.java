a9088ed1d9dc0ead4b6c542c328995ac5aec1ffa
zhangzhl <185378697@qq.com>
2020-03-30 23:46:58 +0800

    字符串中的第一个唯一字符
    Change-Id: I7bcc7069f5cf8633cdd1b31712ec7e86f3ddad34
    Signed-off-by: zhangzhl <185378697@qq.com>

---
+++ src/item/difficulty/easy/FirstUniqChar.java
@@ -0,0 +1,23 @@
+package item.difficulty.easy;
+
+/**
+ * 387. 字符串中的第一个唯一字符
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ * @author zhangzhl
+ *
+ */
+public class FirstUniqChar {
+
+	public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for(int i = 0;i < s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int i = 0;i < s.length();i++){
+            if(freq[s.charAt(i)-'a'] == 1){
+                return i;
+            }
+        }
+        return  -1;
+    }
+}
