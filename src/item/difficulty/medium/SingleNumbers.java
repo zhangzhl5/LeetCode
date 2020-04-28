f17f5c43a91d85ff5db46324c32effeafbb2a8c4
zhangzhl5 <zhangzhl5@yonyou.com>
2020-04-28 13:30:28 +0800

    a&(-a)=最低位为1的二进制（从右到左）
    Change-Id: I323aa5a6dc58b07ccf41bbdf9dae9a7c5d9ebf69
    Signed-off-by: zhangzhl5 <zhangzhl5@yonyou.com>

--- src/item/difficulty/medium/SingleNumbers.java
+++ src/item/difficulty/medium/SingleNumbers.java
@@ -2,20 +2,16 @@ package item.difficulty.medium;
 
 /**
  * 面试题56 - I. 数组中数字出现的次数
- * 
- * @author Administrator
- *
  */
 public class SingleNumbers {
 
 	/**
 	 * 
-	 * 异或的性质:两个数字异或的结果a^b是将 a 和 b 的二进制每一位进行运算，得出的数字。
-	 * 运算的逻辑是 如果同一位的数字相同则为 0，不同则为 1
-	 * 异或的规律
-	 * 任何数和本身异或则为0
-	 * 任何数和 0 异或是本身
-	 * 异或满足交换律。 即 a ^ b ^ c ，等价于 a ^ c ^ b
+	 * 相同的数异或为0，不同的异或为1。0和任何数异或等于这个数本身。
+	 * 所以，数组里面所有数异或 = 目标两个数异或 。 由于这两个数不同，所以异或结果必然不为0。
+	 * 假设数组异或的二进制结果为10010，那么说明这两个数从右向左数第2位是不同的
+	 * 那么可以根据数组里面所有数的第二位为0或者1将数组划分为2个。
+	 * 这样做可以将目标数必然分散在不同的数组中，而且相同的数必然落在同一个数组中。
 	 * @param nums
 	 * @return
 	 */
@@ -23,22 +19,19 @@ public class SingleNumbers {
 
 		// 用于将所有的数异或起来
 		int k = 0;
-
 		for (int num : nums) {
 			k ^= num;
 		}
-
 		// 获得k中最低位的1
+		// 假设数组异或的二进制结果为10010，那么说明这两个数从右向左数第2位是不同的
 		int mask = 1;
-
-		// mask = k & (-k) 这种方法也可以得到mask，具体原因百度 哈哈哈哈哈
+		// mask = k & (-k) 这种方法也可以得到mask
+		// a&(-a)=最低位为1的二进制（从又到左）
 		while ((k & mask) == 0) {
 			mask <<= 1;
 		}
-
 		int a = 0;
 		int b = 0;
-
 		for (int num : nums) {
 			if ((num & mask) == 0) {
 				a ^= num;
@@ -46,9 +39,7 @@ public class SingleNumbers {
 				b ^= num;
 			}
 		}
-
 		return new int[] { a, b };
-
 	}
 
 }
