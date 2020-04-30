8d53e9df8bd938ab5203443128f2b8e03ce3fbe1
zhangzhl5 <zhangzhl5@yonyou.com>
2020-04-30 17:23:21 +0800

    二分搜索
    Change-Id: Ibb02c84b2137b9d8da2d6e4b4981680c0f0b04aa
    Signed-off-by: zhangzhl5 <zhangzhl5@yonyou.com>

---
+++ src/item/difficulty/easy/SearchInsert.java
@@ -0,0 +1,21 @@
+package item.difficulty.easy;
+
+/**
+ * 35. 搜索插入位置
+ * @author Administrator
+ *
+ */
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+    	int leftindex  = 0;
+    	int index  = 0;
+    	int left = 0;
+    	int n = nums.length;
+    	int right = n - 1;
+    	while(left < right) {
+    		
+    	}
+		return target;
+
+    }
+}
