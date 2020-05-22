a5104b0037e090acae65aa8da3295b319a839d5b
zhangzhl <185378697@qq.com>
2020-05-22 22:48:42 +0800

    构造二叉树、
    Change-Id: I4f89ea97ddcd3002c045ec253609dcedd2190ac9
    Signed-off-by: zhangzhl <185378697@qq.com>

---
+++ src/item/difficulty/medium/BuildTree.java
@@ -0,0 +1,22 @@
+package item.difficulty.medium;
+
+/**
+ * 105. 从前序与中序遍历序列构造二叉树
+ * @author zhangzhl
+ *
+ */
+public class BuildTree {
+	
+	public class TreeNode {
+		    int val;
+		    TreeNode left;
+		    TreeNode right;
+		 TreeNode(int x) { val = x; }
+	}
+	
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+		return null;
+    	
+    }
+
+}
