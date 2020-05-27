3c9631f37e71b710c1f530678599197a79d2f050
zhangzhl5 <zhangzhl5@yonyou.com>
2020-05-27 15:22:27 +0800

    和可被 K 整除的子数组
    Change-Id: I3fd848e6b8ef611dd32cb4d4a5166a8ddb8fe544
    Signed-off-by: zhangzhl5 <zhangzhl5@yonyou.com>

---
+++ src/item/difficulty/medium/SubarraysDivByK.java
@@ -0,0 +1,29 @@
+package item.difficulty.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 974. 和可被 K 整除的子数组
+ * 给定一个整数数组 A，返回其中元素之和可被 K 整除的（连续、非空）子数组的数目。
+ * @author zhangzhl
+ *
+ */
+public class SubarraysDivByK {
+	
+    public int subarraysDivByK(int[] A, int K) {
+    	Map<Integer, Integer> record = new HashMap<>();
+        record.put(0, 1);
+        int sum = 0, ans = 0;
+        for (int elem: A) {
+            sum += elem;
+            // 注意 Java 取模的特殊性，当被除数为负数时取模结果为负数，需要纠正
+            int modulus = (sum % K + K) % K;
+            int same = record.getOrDefault(modulus, 0);
+            ans += same;
+            record.put(modulus, same + 1);
+        }
+        return ans;
+    }
+
+}
